1713a92c40a9e9a4302a76cef157c21ab780ba85
abdkhaleel <abdkhaleel16@gmail.com>
2025-12-17 00:16:25 +0530

    terraform infra

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0" 
+        }
+    }
+}
+
+provider "aws" {
+    region                      = "us-east-1"
+    access_key                  = "test"
+    secret_key                  = "test"
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+
+    s3_use_path_style           = true 
+
+    endpoints {
+        s3  = "http://localhost:4566"
+        sns = "http://localhost:4566"
+    }
+}
+
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "zoho-crash-logs-archive"  
+}
+
+resource "aws_sns_topic" "alert_topic" {
+  name = "zoho-critical-alerts"
+}
